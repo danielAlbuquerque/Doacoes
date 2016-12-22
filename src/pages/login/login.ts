73a79a6ed19462b3f1d4dd478d41a0fb4414a051
daniel.alb.querque@gmail.com <daniel.alb.querque@gmail.com>
2016-12-22 15:48:10 -0300

    Form login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams, LoadingController, AlertController } from 'ionic-angular';
+import { NavController, NavParams, LoadingController, AlertController, Loading } from 'ionic-angular';
 import { FormBuilder, Validators } from '@angular/forms';
+
+import { RegistrarPage } from '../registrar/registrar';
+import { RecuperarSenhaPage } from '../recuperar-senha/recuperar-senha';
 
 @Component({
   selector: 'page-login',
@@ -8,6 +11,10 @@ import { FormBuilder, Validators } from '@angular/forms';
 })
 export class LoginPage {
 	loginForm: any;
+	emailChanged: boolean = false;
+  	passwordChanged: boolean = false;
+  	submitAttempt: boolean = false;
+	loading: Loading;	
 
 	constructor(
 	  	public navCtrl: NavController, 
@@ -22,6 +29,62 @@ export class LoginPage {
       	});
   	}
 
-  
+  	/** Realiza o login através de email/senha */
+  	loginEmail() {
+  		this.submitAttempt = true;
+  		if(!this.loginForm.valid) {
+  			console.log(this.loginForm.value);
+  		} else {
+  			this.showLoading();
+  			// Todo: Implementar o registro
+  		}
+  	}
+
+  	/** Envia o usuário para a tela de criar conta */
+  	criarConta() {
+  		this.navCtrl.push(RegistrarPage);	
+  	}
+
+  	/** Envia o usuário para tela de resetar senha */
+  	recuperarSenha() {
+  		this.navCtrl.push(RecuperarSenhaPage);
+  	}
+
+
+  	/** Realiza o login através do facebook */
+  	loginFacebook() {
+  		// Todo: Implementar essa function
+  	}
+
+  	/** Não sei oq essa porra faz mas precisa ta aqui */
+  	elementChanged(input){
+    	let field = input.inputControl.name;
+    	this[field + "Changed"] = true;
+  	}
+
+  	
+  	/** Exibe o popup loading */
+  	private showLoading() {
+		this.loading = this.loadingCtrl.create({
+			content: 'Aguarde...'
+		});
+		this.loading.present();
+	}
+
+	/** Exibe uma mensagem de erro ao usuário */
+	private showError(text) {
+		setTimeout(() => {
+			this.loading.dismiss();
+		});
+
+		let alert = this.alertCtrl.create({
+			title: 'Fail',
+			subTitle: text,
+			buttons: ['OK']
+		});
+
+		alert.present(prompt);
+	}
+
 
 }
