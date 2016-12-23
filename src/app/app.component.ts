e14b3a2ba0b11f8d7940d5f98b82984d5ad9f13f
daniel.alb.querque@gmail.com <daniel.alb.querque@gmail.com>
2016-12-23 09:32:07 -0300

    Impl logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,86 @@
 import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
-
+import { AlertController, LoadingController, Loading } from 'ionic-angular';
 import { LoginPage } from '../pages/login/login';
 import { RegistrarPage } from '../pages/registrar/registrar';
-
+import { AuthProvider } from '../providers/auth';
 
 
 @Component({
-  templateUrl: 'app.html'
+  templateUrl: 'app.html',
+  providers: [AuthProvider]
 })
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
   rootPage: any = LoginPage;
+  loading: Loading;
 
-  constructor(public platform: Platform) {
+  /**
+   * Construtor
+   * @param {Platform}          public  platform    [description]
+   * @param {AlertController}   private alertCtrl   [description]
+   * @param {LoadingController} public  loadingCtrl [description]
+   * @param {AuthProvider}      private auth        [description]
+   */
+  constructor(
+    public platform: Platform, 
+    private alertCtrl: AlertController, 
+    public loadingCtrl: LoadingController,
+    private auth: AuthProvider
+  ) {
     this.initializeApp();
-
-
-
   }
 
+  /**
+   * Platform is ready
+   */
   initializeApp() {
     this.platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
       Splashscreen.hide();
     });
   }
 
-  openPage(page) {
-    // Reset the content nav to have just this page
-    // we wouldn't want the back button to show in this scenario
-    this.nav.setRoot(page.component);
+  /**
+   * Logout
+   */
+  logout() {
+    let confirm = this.alertCtrl.create({
+      title: 'Doações',
+      message: 'Deseja realmente sair?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          handler: () => {
+            
+          }
+        },
+        {
+          text: 'Sim',
+          handler: () => {
+            this.showLoading();
+            this.auth.logout().subscribe(() => {
+              setTimeout(() => {
+                this.loading.dismiss();
+                this.nav.setRoot(LoginPage);
+              }, 2000);
+            });
+          }
+        }
+      ]
+    });
+    confirm.present();
+  }
+
+  /**
+   * Exibe o popup loading
+   */
+  private showLoading() {
+    this.loading = this.loadingCtrl.create({
+        content: 'Saindo...'
+    });
+    this.loading.present();
   }
 }
