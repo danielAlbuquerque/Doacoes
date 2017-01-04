476583edc0cb8f365355f337414f40c8999d33e7
Daniel Albuquerque <daniel.alb.querque@gmail.com>
2017-01-04 10:22:08 -0300

    impl detalhes

--- src/pages/doacao-detalhe/doacao-detalhe.ts
+++ src/pages/doacao-detalhe/doacao-detalhe.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild, ViewChildren, QueryList, ElementRef } from '@angular/core';
-import { NavController, Slides, NavParams, LoadingController, AlertController, Loading } from 'ionic-angular';
+import { NavController, Slides, NavParams, LoadingController, AlertController, Loading, ActionSheetController } from 'ionic-angular';
 import { DoacaoProvider } from '../../providers/doacao';
 
 @Component({
@@ -11,29 +11,19 @@ export class DoacaoDetalhePage {
 	@ViewChildren('slide') slides: QueryList<ElementRef>;
   @ViewChild('slider') slider: Slides;
 
-  public _robots: any[];
   public _sliderOptions: any;
   public loading: Loading;
   public id: string = null;
-  public doacao: any;
+  public doacao: any = {usuario: {}};
 
 	constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
     public loadingCtrl: LoadingController,
     public alertCtrl: AlertController,
-    public doacaoProvider: DoacaoProvider
+    public doacaoProvider: DoacaoProvider,
+    public actionSheetCtrl: ActionSheetController
   ) {
-		this._robots = [[],[],[]];           
-		
-    var i;
-    
-    for(i = 1; i <= 5; i++)             // 1st element will have 5 items
-      this._robots[0].push(i);
-    for(i = 1; i <= 15; i++)             // 2nd element will have 15 items
-      this._robots[1].push(i);
-    for(i = 1; i <= 25; i++)             // 1st element will have 25 items
-      this._robots[2].push(i);
 
     this._sliderOptions = {
       pager: true
@@ -42,6 +32,28 @@ export class DoacaoDetalhePage {
     this.id = this.navParams.get('id');
 
 	}
+
+  share() {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'Compartilhar',
+      buttons: [
+        {
+          text: 'Facebook',
+          icon: 'logo-facebook',
+          handler: () => {
+            console.log('Destructive clicked');
+          }
+        },{
+          text: 'Whatsapp',
+          icon: 'logo-whatsapp',
+          handler: () => {
+            console.log('Archive clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
+  }
 
 	ionViewDidLoad() {
     this.showLoading("Carregando...");
@@ -54,6 +66,34 @@ export class DoacaoDetalhePage {
       this.loading.dismiss();
     });
 	}
+
+  denunciar() {
+    let prompt = this.alertCtrl.create({
+      title: 'Denunciar publicação',
+      message: "Descreva a denuncia",
+      inputs: [
+        {
+          name: 'Denuncia',
+          placeholder: ''
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          handler: data => {
+            
+          }
+        },
+        {
+          text: 'Salvar',
+          handler: data => {
+            
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
 
 
   /**
@@ -89,15 +129,17 @@ export class DoacaoDetalhePage {
   }
 
 	SlideChanged() {
-    let currentSlide = this.slider.getActiveIndex();
-    let slideElements = this.slides.toArray();
+    //let currentSlide = this.slider.getActiveIndex();
+    //let slideElements = this.slides.toArray();
 
-    let targetSlide = slideElements[currentSlide];
+
+
+    //let targetSlide = slideElements[currentSlide];
     //console.log(targetSlide);
-    let height = targetSlide['ele']['children'][0]['clientHeight'];
+    //let height = targetSlide['ele']['children'][0]['clientHeight'];
     //console.log('target slide height = ' + height);
 
-    this.slider._elementRef.nativeElement.style.height = height + 'px';
+    //this.slider._elementRef.nativeElement.style.height = height + 'px';
   }
 
 }
--- src/pages/ver-doacoes/ver-doacoes.ts
+++ src/pages/ver-doacoes/ver-doacoes.ts
@@ -54,7 +54,7 @@ export class VerDoacoesPage {
   }
 
   detalhesDoacao(id) {
-    this.navCtrl.push(DoacaoDetalhePage, {id: id});
+    this.app.getRootNav().push(DoacaoDetalhePage, {id: id});
   }
 
   /**
